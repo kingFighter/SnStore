07290b6e7c4cd5d4c3908a5bfa18d7ccc401cf40
PrinC <principle0720@gmail.com>
2014-06-04 11:16:13 +0800

    directory

---
+++ snstore/src/snstore.h
@@ -0,0 +1,16 @@
+#ifndef SNSTORE_H
+#define SNSTORE_H
+#include <vector>
+#include <string>
+
+using namespace std;
+namespace Kevin {
+  class SnStore 
+  public:
+    SnStore();
+    string get(int key);
+    void put(int key, string value);
+    vector<string> getRange(int minKey, int maxKey);
+  };
+}
+#endif
