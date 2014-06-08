00fbb62810e94890473e825b7d44b9aa83efc572
Kaiyang Lv(Kevin Lui) <kevinlui598@gmail.com>
2014-06-08 12:04:51 +0800

    Split requests

--- snstore/src/server/coordinator.cpp
+++ snstore/src/server/coordinator.cpp
@@ -4,9 +4,19 @@ std::map<int,std::string> db;
 
 Coordinator::Coordinator(int worker_num, int down_, int up_) : down(down_), up(up_)
 {
-    txid = 1;
-    for (int i = 0; i < worker_num; ++i) 
-      workers.push_back(Worker());
+  txid = DEFAULT_TX_ID;
+  holder = DEFAULT_TX_ID;
+  size = (up - down) / (worker_num - 2);
+  // worker[0]: (~,down)
+  // worker[1]: [down, down + size)
+  // worker[2]: [down + size, down + 2 * size)
+  // worker[worker_num - 2]: [up,~)
+  // conditions.resize(worker_num);
+  for (int i = 0; i < worker_num; ++i) {
+    workers.push_back(Worker());
+    operations.push_back(queue<string>());
+    results.push_back(queue<string>());
+  }
 }
 
 Coordinator::~Coordinator()
@@ -16,7 +26,9 @@ Coordinator::~Coordinator()
 void
 Coordinator::begin(RpcController* controller, const BeginRequest* request, BeginResponse* response, Closure* done)
 {
-    response->set_txid(txid++);
+  if (holder == DEFAULT_TX_ID)
+    ++holder;
+    response->set_txid(++txid);
     done->Run();
 }
 
@@ -24,9 +36,68 @@ void
 Coordinator::execTx(RpcController* controller, const TxRequest* request, TxResponse* response, Closure* done)
 {
   int32 txidReqs = request->txid();
-  
+  // get transaction lock
+  {
+    boost::mutex::scoped_lock lock(global_mutex);
+    while (txidReqs != holder)
+      global_con.wait(lock);
+  }
+  {
+    boost::mutex::scoped_lock lock(request_mutex);
     RepeatedPtrField<TxRequest_Request> reqs = request->reqs();
     RepeatedPtrField<TxRequest_Request>::iterator it = reqs.begin();
+    
+    // split request
+    set<int> operationsWakeup;
+    for(;it != reqs.end(); it++) {
+      switch (it->op()) {
+      case TxRequest_Request::GET: {
+        int getkey = it->key1();
+        ++num;
+        string str = "get " + int2string(getkey);
+        int pos = getPos(getkey);
+        operationsWakeup.insert(pos);
+        (operations[pos]).push(str);
+        break;
+      }
+      case TxRequest_Request::PUT: {
+        int putkey = it->key1();
+        ++num;
+        string str = "put " + int2string(putkey);
+        int pos = getPos(putkey);
+        operationsWakeup.insert(pos);
+        (operations[pos]).push(str);
+        break;
+      }
+      case TxRequest_Request::GETRANGE: {
+        int minkey = it->key1();
+        int maxkey = it->key2();
+        // number and split
+        int posMin = getPos(minkey);
+        int posMax = getPos(maxkey);
+        num += posMax - posMin + 1;
+        // Split getRange
+        string str = "getRange ";
+        if (posMin != posMax) {
+          (operations[posMin]).push(str + int2string(minkey) + " " + int2string(posMin * size - 1));
+          operationsWakeup.insert(posMin);
+          (operations[posMax]).push(str + int2string((posMax - 1) * size) + " " + int2string(maxkey));
+          operationsWakeup.insert(posMax);
+        } else {
+          operations[posMin].push(str + int2string(minkey) + " " + int2string(maxkey));
+          operationsWakeup.insert(posMin);
+        }
+        
+        for (int i = posMin + 1; i <= posMax; ++i) {
+          operationsWakeup.insert(i);
+          (operations[i]).push(str + int2string((i - 1) * size ) + " " + int2string(i * size - 1));
+        }
+        
+        break;
+      }
+      }
+    }
+
     for(;it != reqs.end(); it++) {
         switch (it->op()) {
             case TxRequest_Request::GET:
@@ -65,4 +136,5 @@ Coordinator::execTx(RpcController* controller, const TxRequest* request, TxRespo
         }
     }
     done->Run();
+  }
 }
--- snstore/src/server/coordinator.h
+++ snstore/src/server/coordinator.h
@@ -10,6 +10,7 @@
 #include "../utility/utility.h"
 #include "worker.h"
 #include <boost/thread.hpp>
+#include <queue>
 
 using namespace google::protobuf;
 
@@ -21,14 +22,43 @@ public:
     void execTx(RpcController* controller, const TxRequest* request, TxResponse* response, Closure* done);
 
 private:
+    /* give each client a transactgion id*/
     int txid;
+    /* holder has the global lock */
+    int holder;
     int down;
     int up;
+    /* size = (up - down) / (worker_num - 2) */
+    int size; 
+    /* the request number */
+    int num;
     vector<Worker> workers;
-//
-//	int local_execTx(std::string args, Transaction r);
+    /* each worker has a operations queue, a result queue and an */
+    /* operation queue condition  */
+    vector<queue<string> > operations;
+    vector<queue<string> > results;
+    vector<boost::condition_variable> conditions;
+    /* global_mutex used as global lock */
+    boost::mutex global_mutex;
+    boost::mutex results_mutex;
+    boost::mutex request_mutex;
+    boost::condition_variable request_con;
+    boost::condition_variable results_con;
+    boost::condition_variable global_con;
+    int getPos(int key) {
+      if (key < down) {
+        return 0;
+      } else if (key >= up) {
+        return workers.size() - 1;
+      } else {
+        return (key / size + 1);
+      }
+      
+    }
 };
 #endif
+
+
 
 
 
